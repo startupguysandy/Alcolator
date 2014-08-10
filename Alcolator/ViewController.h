06c1bba865282cf04cc8f8a73c20a2bd08faa881
startupguysandy <andy@thestartupguys.co.uk>
2014-08-10 15:16:38 +0100

    Initial Commit

---
+++ Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Andy Bradbury on 10/08/2014.
+//  Copyright (c) 2014 Tin Bear Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
